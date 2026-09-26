8ac6250863e48b4d954eca4a0b3fc22b70ecb581
Patricio-Montes <montes_patricio@yahoo.com.ar>
2026-09-25 22:47:10 -0300

    fix(tests): import the hosting extensions the system administrator seed test uses

--- tests/Commerce.Integration/TestSeedSystemAdminTests.cs
+++ tests/Commerce.Integration/TestSeedSystemAdminTests.cs
@@ -2,6 +2,7 @@ using System.Net;
 using System.Net.Http.Json;
 using System.Text.Json;
 using Commerce.Cloud.Api.Endpoints;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Npgsql;
 
